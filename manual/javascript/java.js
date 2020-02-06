ffe55aeb5b5e895266948303729514d386e5881f
sharon kim <sharonkim@sharons-mbp-2.wireless.newschool.edu>
2020-02-05 21:19:09 -0500

    manual-2

---
+++ manual/javascript/java.js
@@ -0,0 +1,25 @@
+// get all of the divs that have a class name "step"
+document.querySelectorAll(".step").forEach(function(step) {
+  // make an event handler. this happens on press
+  step.onclick = function () {
+    // get an array (list) of all the children of this div
+    Array.from(step.childNodes)
+      // "filter" get only the <p> elements
+      .filter(function(el) { return el.tagName === "P"; })
+      // for every <p> element, do the thing below
+      .forEach(function(p) {
+        // if the p is displayed make it hidden and visa versa
+
+        if (p.style.display === "block") {
+          p.style.display = "none";
+        } else {
+          p.style.display = "block";
+        }
+
+      });
+  }
+})
+function play() {
+        var audio = document.getElementById("audio");
+        audio.play();
+      }
