69b6deb6ce71afcc0764bbc0110549f66cb45b69
sharonyeunkim <sharonyeunkim@gmail.com>
2020-04-10 12:18:20 -0400

    attempt to load json file to html

--- Collection_1 copy/app.js
+++ Collection_1 copy/app.js
@@ -1,19 +1,25 @@
+/**** WHAT WE'RE DOING IN THIS FILE ****/
+//1. Creating some dummy data for the example. For the actual project, replace this with the json fetch code
+//2. Keep track of which recipe "tags" the user has selected
+//3. Dynamically filter through your JSON data and show elements that match the user's tags
 
+/**** STEP 0: SELECTING ELEMENTS ****/
 var labels = document.querySelectorAll("label"); //all the label elements the user can click on
 var recipeContainer = document.getElementById("recipe-container"); //the div we're going to place the filtered recipe results into
 
-var json = "data.json";
-fetch("data.json")
-      .then(function(blob){ return blob.json(); }) //what does .then mean?
-      .then(function(json){ buildPage(json); });
+/****************************************************/
+/**** STEP 1: FETCHING DATA & STARTING TO USE IT ****/
+/****************************************************/
+//NOTE: Replace this step with the JSON fetch functions once you're ready to integrate the data in your excel file
 
-  function buildPage(data) {
-    console.log(data);
-  }
+//Example data. Your excel file will be converted into an array of JavaScript objects, where each row on the spreadsheet
+//         will be a unique JavaScript object. Each column "name, image, tags, etc." will become a property on the object and the data in the associated cell be the value of the property.
+var json = ("data.json");
+console.log(json);
 
- var json = '{"RecipeTitle": "", "Type": "", "Taste": "", "Attributes": "",}';
-// function splitData(data) {
-//
+// var data = ('dataCopy.json');
+// function realData(data) {
+//   var json = '{"RecipeTitle": "", "Type": "", "Taste": "", "Attributes"; "",}';
 // }
 // var json = data
 // var data = [
