fb06294c03fdfd62195d4ac2963efd705ba956a2
sharonyeunkim <sharonyeunkim@gmail.com>
2020-04-17 15:27:42 -0400

    trying to change inner HTML to createElement

--- Collection_7/recipe.js
+++ Collection_7/recipe.js
@@ -1,7 +1,7 @@
 var data;
 var selectedTags = [];
 var labels = document.querySelectorAll("label");
-var recipeList = document.getElementById("recipeList");
+// var recipeList = document.getElementById("recipeList");
 var recipes = document.getElementById("recipes");
 var recipeTemplate = document.getElementById("recipeTemplate");
 var recipeInstructioncontainer = document.getElementById("recipeInstructioncontainer");
@@ -35,67 +35,48 @@ function toggleLabelSelection(event) {
 // *******************************************************
 function showRecipes() {
  var selectedRecipes = [];
-  data.forEach(function (recipe) {  //could name it anything besides recipie.
+  data.forEach(function (recipe) {  //could name it anything besides recipe.
     selectedTags.forEach(function (tag) {
       if (recipe.tags.includes(tag)) {
         selectedRecipes.push(recipe);
         return;
       }
     });
-  }); // ***** below pushes it into HTML  ****
+  });
   recipes.innerHTML = "";
-  selectedRecipes.forEach(function (recipe) {   //current recipe is dynamically linked
-    var recipeLink = `
-    <div class="recipe-link">
-      <img src="${recipe.images}">
-      <h1>${recipe.recipeTitle}</h1>
-      <p>${recipe.tags}</p>
-    </div>
-    `;    // lines 48 to 54 must be recoded using create element and append child. Then you can do
-    //          on click handler for each thing in loop.
-    recipes.innerHTML += recipeLink;
-    // alternatively use createelement and append child to build DIV above.
-    //  because h1.onclick = function() this can be the function to click when I call my title
-    //  this function needs to clear recipe.inner.html"" (line 46) then does the same thing again
-         //    rebuilds page with this full one screen page of recipe instructions.
-
+  selectedRecipes.forEach(function (recipe) {
+    var DOMrecipeListContainer = document.createElement('div');
+    DOMrecipeListContainer.class = "recipe-link";
+    var img = document.createElement('img'); DOMrecipeListContainer.style.width = "100VW";
+    img.setAttribute('src', recipe.images);
+    DOMrecipeListContainer.appendChild(img);
+    DOMrecipeListContainer.style.height = "100VW";
+    DOMrecipeListContainer.style.background = "green";
+    document.body.appendChild(DOMrecipeListContainer);
   });
   showrecipeInstructioncontainer();
 }
+
+
+// var recipeLink = `
+// <div class="recipe-link">
+//   <img src="${recipe.images}">
+//   <h1>${recipe.recipeTitle}</h1>
+//   <p>${recipe.tags}</p>
+// </div>
+// `;          // lines 48 to 54 must be recoded using create element and append child. Then you can do
+//             //          on click handler for each thing in loop.
+// recipes.innerHTML += recipeLink;
+//             // alternatively use createelement and append child to build DIV above.
+//            //  because h1.onclick = function() this can be the function to click when I call my title
+//            //  this function needs to clear recipe.inner.html"" (line 46) then does the same thing again
+//             //    rebuilds page with this full one screen page of recipe instructions.
+
+
 //*****************************************************
 //********* Document Clicked Individual Recipe ***********
 // *******  3) No, actually record data again**************************************
 // *******************************************************
-//
-// function showrecipeInstructioncontainer() {
-// var showTHATrecipe = [];
-//  var selectedRecipe = event.target.recipeLink;
-//   recipeLink.forEach(function (recipe) {  //could name it anything besides recipie.
-//     selectedRecipe.addEventListener("click")
-//     {
-//       if (selectedRecipe."click" == true) {
-//         showTHATrecipe.push(recipe);
-//         return;
-//       };
-//       showTHATrecipe(recipeLink);
-//     });
-//   };
-//
-// //*****************************************************
-// //********* SHOW THAT Clicked Individual Recipe ***********
-// // ************************************************
-// function showTHATrecipe(recipeLink) {
-// showTHATrecipe.forEach(function (recipe) {   //current recipe is dynamically linked
-//   var recipeInstruction = `
-//   <div class="recipe-link">
-//     <img src="${recipe.images}">
-//     <h1>${recipe.recipeTitle}</h1>
-//     <p>${recipe.tags}</p>
-//   </div>
-//   `;
-//   showTHATrecipe.innerHTML += recipeInstruction;
-// });
-// };
 
 //*****************************************************
 // ************************************************
